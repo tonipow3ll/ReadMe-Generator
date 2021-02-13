c5af2e436ad632d72c460e9a3c71f04545e11e7d
Toni Powell <tonipow3ll@gmail.com>
2021-02-13 14:06:21 -0500

    udpated git ignore

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const createReadme = (answers) => 
+const createReadMe = (answers) => 
 `# ${answers.title} 
-# [View the Deployed Application Here](${answers.deployed})
-# [ScreenShot](${answers.link})
 ### ${answers.description}
+# ![ScreenShot](${answers.link})
+# [View the Deployed Application Here](${answers.deployed})
 ### ${answers.technologies}
 
-#Installation
+# Installation
 Clone repository to your local device
 Test by opening the index.html file in your browser. If it looks good, it's working right.
 If it does not work, right-click Inspect console to determine if there is an error. 
 
-#Credits
+# Credits
 ### UNH Full Stack Development Bootcamp
 
-#Contributors
+# Contributors
 ### ${answers.contributors}
 
 
-#Further Questions / Contact:
-###[LinkedIn](${answers.linkedin})
-###[Email](${answers.email})
-###[GitHub](${answers.github}.github.io)
+# Further Questions / Contact:
+### [LinkedIn](${answers.linkedin})
+### [Email](${answers.email})
+### [GitHub](${answers.github}.github.io)
 
-#Licenses
-### Copyright (c) [${answers.year}] [${answers.name}]
+# Licenses
+### Copyright (c) ${answers.year} ${answers.contributors}
+### ${answers.choices}
 `
 
 inquirer
@@ -76,5 +77,24 @@ inquirer
             name: 'github',
             message: 'enter your github user name'
         },
-
+        {
+            type: 'input',
+            name: 'year',
+            message: 'what is the current year, or year this was created?'
+        },
+        {
+            type: 'checkbox',
+            message: 'please choose a license for this project',
+            name: 'license',
+            choices: ['Apache License 2.0', 'GNU General Public License', 'MIT License', 'BSD 2-Clause "Simplified LIcense', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Create Commons Zero v1.0 Universal', 'Eclipse Public License', 'GNU Affero GEneral Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public LIcense v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+        },
     ])
+    .then((answers) => {
+        const readme = createReadMe(answers);
+
+        fs.writeFile('README.md', readme, (err) =>
+        err ? console.log(err) : console.log('"It always seems impossible until its done...." README.md created! nice job.'))
+    })
+
+
+   
